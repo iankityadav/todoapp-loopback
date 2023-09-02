00f522efac63210f2a0c1549280fc48e096b95f0
iankityadav <1002ankit@gmail.com>
2023-09-02 17:15:41 +0530

    user service for jwt token verification

---
+++ src/services/user.service.ts
@@ -0,0 +1,95 @@
+import {UserService} from '@loopback/authentication';
+import {Credentials} from '@loopback/authentication-jwt';
+import {repository} from '@loopback/repository';
+import {HttpErrors} from '@loopback/rest';
+import {securityId, UserProfile} from '@loopback/security';
+import {compare} from 'bcryptjs';
+import {CustomCredentials, User, UserWithRelations} from '../models';
+import {UserRepository} from '../repositories';
+
+export class MyUserService implements UserService<User, Credentials> {
+  constructor(
+    @repository(UserRepository) public userRepository: UserRepository,
+  ) {}
+  async verifyCredentials(credentials: Credentials): Promise<User> {
+    const invalidCredentialsError = 'Invalid email or password.';
+
+    const foundUser = await this.userRepository.findOne({
+      where: {email: credentials.email},
+    });
+    if (!foundUser) {
+      throw new HttpErrors.Unauthorized(invalidCredentialsError);
+    }
+
+    const credentialsFound = await this.userRepository.findCredentials(
+      foundUser.id,
+    );
+    if (!credentialsFound) {
+      throw new HttpErrors.Unauthorized(invalidCredentialsError);
+    }
+
+    const passwordMatched = await compare(
+      credentials.password,
+      credentialsFound.password,
+    );
+
+    if (!passwordMatched) {
+      throw new HttpErrors.Unauthorized(invalidCredentialsError);
+    }
+
+    return foundUser;
+  }
+
+  async verifyCustomCredentials(credentials: CustomCredentials): Promise<User> {
+    const invalidCredentialsError = 'Invalid username or password.';
+
+    const foundUser = await this.userRepository.findOne({
+      where: credentials.email
+        ? {email: credentials.email}
+        : {username: credentials.username},
+    });
+    if (!foundUser) {
+      throw new HttpErrors.Unauthorized(invalidCredentialsError);
+    }
+
+    const credentialsFound = await this.userRepository.findCredentials(
+      foundUser.id,
+    );
+    if (!credentialsFound) {
+      throw new HttpErrors.Unauthorized(invalidCredentialsError);
+    }
+
+    const passwordMatched = await compare(
+      credentials.password,
+      credentialsFound.password,
+    );
+
+    if (!passwordMatched) {
+      throw new HttpErrors.Unauthorized(invalidCredentialsError);
+    }
+
+    return foundUser;
+  }
+
+  convertToUserProfile(user: User): UserProfile {
+    return {
+      [securityId]: user.id.toString(),
+      name: user.name,
+      id: user.id,
+      username: user.username,
+    };
+  }
+
+  //function to find user by id
+  async findUserById(id: string): Promise<User & UserWithRelations> {
+    const userNotfound = 'invalid User';
+    const foundUser = await this.userRepository.findOne({
+      where: {id: id},
+    });
+
+    if (!foundUser) {
+      throw new HttpErrors.Unauthorized(userNotfound);
+    }
+    return foundUser;
+  }
+}
